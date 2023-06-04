d08bff537bbe5adfdd9999c736ea529ce193d729
jjeg1979 <jjeg1979@gmail.com>
2023-06-04 21:15:08 +0200

    Added initial tests to currency package

--- src/currency/money.py
+++ src/currency/money.py
@@ -13,46 +13,47 @@ class Symbol(StrEnum):
     Returns:
         StrEnum: Returns an Enumreation of type 'Symbol'
     """
-    AUD = "AUD",
-    BGN = "BGN",
-    CAD = "CAD",    
-    CHF = "CHF",
-    CZK = "CZK"
-    DKK = "DKK",
-    EUR = "EUR",
-    GBP = "GBP",
-    HKD = "HKD",
-    HUF = "HUF",
-    ILS = "ILS",
-    JPY = "JPY",
-    MXN = "MXN",
-    NOK = "NOK",
-    PLN = "PLN",
-    RON = "RON",
-    RUB = "RUB",
-    SEK = "SEK",
-    SGD = "SGD",
-    TRY = "TRY",
-    USD = "USD",
-    NZD = "NZD",
-    ZAR = "ZAR",
-    
+
+    AUD = ("AUD",)
+    BGN = ("BGN",)
+    CAD = ("CAD",)
+    CHF = ("CHF",)
+    CZK = ("CZK",)
+    DKK = ("DKK",)
+    EUR = ("EUR",)
+    GBP = ("GBP",)
+    HKD = ("HKD",)
+    HUF = ("HUF",)
+    ILS = ("ILS",)
+    JPY = ("JPY",)
+    MXN = ("MXN",)
+    NOK = ("NOK",)
+    PLN = ("PLN",)
+    RON = ("RON",)
+    RUB = ("RUB",)
+    SEK = ("SEK",)
+    SGD = ("SGD",)
+    TRY = ("TRY",)
+    USD = ("USD",)
+    NZD = ("NZD",)
+    ZAR = ("ZAR",)
+
     def __str__(self) -> str:
         """Human-readable representation of the Symbol
 
         Returns:
             str: String with the Symbol
         """
-        return f'{self.name}'
-    
+        return f"{self.name}"
+
     def __len__(self) -> int:
         """The length of this class is the number of symbols it can support
-        
+
         Returns:
             int: number of symbols supported
         """
         return len(Symbol.symbol_supported())
-    
+
     @classmethod
     def symbol_supported(cls) -> list[str]:
         """Returns a list with all the Symbols suppported
@@ -63,26 +64,29 @@ class Symbol(StrEnum):
             list[str]: _description_
         """
         return list(cls.__members__.keys())
-    
+
 
 class InvalidMoneyType(Exception):
     """Raised when the type is not of Money type
 
-    
+
     Args:
-        Exception (InvalidMoneyType): Object type is not compliant to Money type
+        Exception (InvalidMoneyType): Object type is not
+        compliant to Money type
     """
-    def __init__(self, msg: str = "Object type not compliant with Money type") -> None:
-        """Exception initializer        
+
+    def __init__(self, msg: str = "Invalid Money Type") -> None:
+        """Exception initializer
 
         Args:
-            msg (str, optional): Error when object is not from Money type. Defaults to "Object type not compliant with Money type".
+            msg (str, optional): Error when object is not from Money type.
+            Defaults to "Object type not compliant with Money type".
         """
         self.message = msg
         super().__init__(self.message)
 
 
-def count_digits(num: float |Decimal) -> int:
+def count_digits(num: int | float | Decimal) -> int:
     """Counts the number of digits in a given floating-point or Decimal number.
 
     Args:
@@ -95,24 +99,27 @@ def count_digits(num: float |Decimal) -> int:
         ValueError: If the number is not of type float or Decimal.
     """
     if isinstance(num, float):
-        digits = str(num).rstrip('0')
-    elif isinstance(num, Decimal): # type: ignore
-        digits = num.normalize().to_eng_string().rstrip('0')
+        digits = str(num).rstrip("0")
+    elif isinstance(num, int):
+        digits = str(num)
+    elif isinstance(num, Decimal):  # type: ignore
+        digits = num.normalize().to_eng_string().rstrip("0")
 
     else:
-        raise ValueError("Number must be in string, float or int format")
+        raise ValueError("Number must be in Decimal, float or int format")
 
-    if '.' in digits:
+    if "." in digits:
         return len(digits) - 1  # Substract one to discard the decimal point
     else:
         return len(digits)
-    
-    
-def count_decimals(num: float |Decimal):
-    """Counts the total number of decimals in a given floating-point or Decimal number.
+
+
+def count_decimals(num: int | float | Decimal):
+    """Counts the total number of decimals in a given
+        floating-point or Decimal number.
 
     Args:
-        number: A float or Decimal number.
+        number: An int, float or Decimal number.
 
     Returns:
         The total number of decimals in the given number.
@@ -120,20 +127,20 @@ def count_decimals(num: float |Decimal):
     Raises:
         ValueError: If the number is not of type float or Decimal.
     """
-    if isinstance(num, float):
+    if isinstance(num, float) or isinstance(num, int):
         dec = str(num)
-    elif isinstance(num, Decimal): # type: ignore
+    elif isinstance(num, Decimal):  # type: ignore
         dec = num.normalize().to_eng_string()
     else:
         raise ValueError("The number must be of type float or Decimal")
 
-    if '.' in dec:
-        return len(dec.split('.')[-1])
+    if "." in dec:
+        return len(dec.split(".")[-1])
     else:
         return 0
 
 
-@dataclass
+@dataclass(slots=True)
 class Money:
     """Class to represent a currency and the corresponding value.
 
@@ -156,29 +163,33 @@ class Money:
     digits: int = field(default_factory=int)
 
     def __post_init__(self) -> None:
-        """Initialize the Money instance.
+        """Post_initialize the Money instance.
 
         This method is called after the instance has been initialized.
-        It sets the number of digits in the `amount_cents` attribute.
+        It sets the number of digits in the `digits` attribute.
         """
-        self.digits = count_digits(self.amount_cents)
+        self.digits = count_digits(self.amount_cents / 10**self.precision)
 
     @classmethod
-    def mint(cls, amount: Decimal | float, currency_symbol: Symbol = Symbol("EUR")) -> 'Money':
+    def mint(
+        cls, amount: Decimal | float, currency_symbol: Symbol = Symbol("EUR")
+    ) -> Self:
         """Create a new Money instance.
 
-        This method creates a new Money instance using the given `amount` and `currency_symbol`.
-        The `precision` attribute is determined based on the number of decimals in the `amount`.
+        This method creates a new Money instance using the given `amount`
+        and `currency_symbol`.The `precision` attribute is determined
+        based on the number of decimals in the `amount`.
 
         Args:
             amount (Decimal or float): The value of the money.
-            currency_symbol (Symbol, optional): The symbol of the currency. Defaults to "EUR".
+            currency_symbol (Symbol, optional): The symbol of the currency.
+            Defaults to "EUR".
 
         Returns:
             Money: A new instance of the Money class.
         """
         precision: int = count_decimals(amount)
-        return cls(int(amount * (10 ** precision)), precision, currency_symbol)
+        return cls(int(amount * (10**precision)), precision, currency_symbol)
 
     def __str__(self) -> str:
         """Return a string representation of the Money instance.
@@ -186,12 +197,13 @@ class Money:
         Returns:
             str: The string representation of the Money instance.
         """
-        return f"{self.currency_symbol}{self.amount_cents / 100:.2f}"
+        return f"{self.amount_cents / 100:.2f} {self.currency_symbol}"
 
     def __add__(self, other: Self) -> Self:
         """Add two Money instances.
 
-        This method adds two Money instances together if they have the same currency symbol.
+        This method adds two Money instances together if they
+        have the same currency symbol.
 
         Args:
             other (Money): The Money instance to be added.
@@ -200,17 +212,24 @@ class Money:
             Money: The sum of the two Money instances.
 
         Raises:
-            InvalidMoneyType: If the `other` parameter is not an instance of the Money class.
+            InvalidMoneyType: If the `other` parameter is not an
+            instance of the Money class.
         """
-        if isinstance(other, Money) and self.currency_symbol == other.currency_symbol:
-            return Money(self.amount_cents + other.amount_cents, self.precision, self.currency_symbol)
+        if (isinstance(other, Money)
+                and self.currency_symbol == other.currency_symbol):
+            return Money(
+                self.amount_cents + other.amount_cents,
+                self.precision,
+                self.currency_symbol,
+            )
         else:
             raise InvalidMoneyType()
 
     def __sub__(self, other: Self) -> Self:
         """Subtract two Money instances.
 
-        This method subtracts the `other` Money instance from the current Money instance.
+        This method subtracts the `other` Money instance from the current
+        Money instance.
 
         Args:
             other (Money): The Money instance to be subtracted.
@@ -219,9 +238,15 @@ class Money:
             Money: The difference between the two Money instances.
 
         Raises:
-            InvalidMoneyType: If the `other` parameter is not an instance of the Money class.
+            InvalidMoneyType: If the `other` parameter is not an instance
+            of the Money class.
         """
-        if isinstance(other, Money):
-            return Money(self.amount_cents - other.amount_cents, self.precision, self.currency_symbol)
+        if (isinstance(other, Money)
+                and self.currency_symbol == other.currency_symbol):
+            return Money(
+                self.amount_cents - other.amount_cents,
+                self.precision,
+                self.currency_symbol,
+            )
         else:
             raise InvalidMoneyType()
--- tests/unit/test_money.py
+++ tests/unit/test_money.py
@@ -1,3 +1,5 @@
+"""Tests for Money Class"""
+
 # Standard libraries import
 
 # Third party libraries import
@@ -10,27 +12,74 @@ from src.currency.money import (
     InvalidMoneyType,
 )
 
+
 @pytest.fixture
 def symbol_enum() -> Symbol:
     """Returns an object from the symbol string enumeration class"""
     return Symbol("EUR")
 
 
-class TestDummy:
+class TestDummyImports:
     """Class that contains dummy tests to check out-of-package functionality"""
-    
+
     def test_import_are_working_fine(self):
-        """Dummy test to check whether or not imports from src are woking or not"""
+        """Dummy test to check whether or not imports from
+        src are woking or not"""
         pass
 
 
-class TestSymbol:
+class TestSymbolStrEnum:
     """Class that contains the tests for the Symbol String Enumeration Class"""
-        
+
     def test_len_symbol(self):
         assert len(Symbol) == 23
 
 
-class MoneyTest:
+class TestMoneyClass:
     """Class that contains the tests for the Money Class"""
-    pass
+
+    def test_money_init_method(self):
+        """__init__() should return a valid object with correct
+        digits and precision"""
+        # GIVEN an initialized money object
+        # WHEN the digits and precision are queried
+        # THEN returned correct values
+        money = Money(200, 2)
+        assert money.digits == 1
+        assert money.precision == 2
+
+    @pytest.mark.parametrize('actual, expected',
+                             [(Money(100, 2) + Money(200, 2), 300),
+                              (Money(200, 2) + Money(200, 2), 400),
+                              (Money(-100, 2) + Money(300, 2), 200),
+                              (Money(-200, 2) + Money(-100, 2), -300),
+                              (Money(400, 2) + Money(200, 2), 600),
+                              (Money(100, 2) + Money(1200, 2), 1300),
+                              (Money(400, 2) + Money(250, 2), 650),
+                              ])  # type: ignore
+    def test_sum_provides_correct_result(self, 
+                                         actual: Money, 
+                                         expected: int):
+        assert actual.amount_cents == expected
+
+    @pytest.mark.parametrize('actual, expected',
+                             [(Money(100, 2) - Money(200, 2), -100),
+                              (Money(200, 2) - Money(200, 2), 0),
+                              (Money(100, 2) - Money(300, 2), -200),
+                              (Money(200, 2) - Money(100, 2), 100),
+                              (Money(400, 2) - Money(200, 2), 200),
+                              (Money(100, 2) - Money(1200, 2), -1100),
+                              (Money(400, 2) - Money(250, 2), 150),
+                              ])  # type: ignore
+    def test_subtraction_provides_correct_result(self,
+                                                 actual: Money,
+                                                 expected: int):
+        assert actual.amount_cents == expected
+
+    def test_sum_of_money_with_other_type_raises_InvalidMoneyTypeError(self):
+        money1 = Money(100, 2)
+        other = 2
+        with pytest.raises(InvalidMoneyType) as ex:
+            money1 + other  # type: ignore
+        assert ex.type == InvalidMoneyType
+        assert ex.value.args[0] == "Invalid Money Type"
